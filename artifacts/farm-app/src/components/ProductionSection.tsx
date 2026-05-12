45b70cbe01c25d5a2066fbfe738cccfeb6b7bbe1
sambelfarmlab <ghiyaskahsyai@gmail.com>
2026-05-12 09:52:37 +0700

    Create ProductionSection.tsx

---
+++ artifacts/farm-app/src/components/ProductionSection.tsx
@@ -0,0 +1,15 @@
+interface ProductionSectionProps {
+  displayData: any;
+  areas: any[];
+}
+
+export function ProductionSection({
+  displayData,
+  areas,
+}: ProductionSectionProps) {
+  return (
+    <div>
+      Production Section
+    </div>
+  );
+}
