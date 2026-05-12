1a01e878534860a778b87b63b6109f234ec5c082
sambelfarmlab <ghiyaskahsyai@gmail.com>
2026-05-12 23:49:19 +0700

    Update FinancialSection.tsx

--- artifacts/farm-app/src/components/FinancialSection.tsx
+++ artifacts/farm-app/src/components/FinancialSection.tsx
@@ -194,7 +194,7 @@ export function FinancialSection({
 </div>
         </CardHeader>
 
-<CardContent>
+<CardContent className="pt-1">
   <div
     className={`
       text-[clamp(1rem,3.2vw,1.6rem)]
