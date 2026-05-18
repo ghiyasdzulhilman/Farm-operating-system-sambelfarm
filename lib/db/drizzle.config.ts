2932de67b0ea86997cc20fa74a348284c7779de9
sambelfarmlab <ghiyaskahsyai@gmail.com>
2026-05-18 16:52:32 +0700

    Update drizzle.config.ts

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "drizzle-kit";
-import path from "path";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
 export default defineConfig({
-  schema: path.join(__dirname, "./src/schema/index.ts"),
+  // ✨ PERBAIKAN: Langsung scan seluruh file di dalam folder schema
+  schema: "./src/schema/*", 
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
