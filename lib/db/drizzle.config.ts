7c826ae6dd60239e6727fe0dc16b6f7155f5cc9b
Ghiyas Dzul hilman <ghiyasdzulhilmansh@gmail.com>
2026-06-18 21:29:40 +0000

    Rollback

--- lib/db/drizzle.config.ts
+++ lib/db/drizzle.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from "drizzle-kit";
+import path from "path";
 
 if (!process.env.DATABASE_URL) {
-  throw new Error("DATABASE_URL is required");
+  throw new Error("DATABASE_URL, ensure the database is provisioned");
 }
 
 export default defineConfig({
-  schema: "./src/schema.ts",
-  out: "./drizzle",
+  schema: "./src/**/*", // ✨ Cari semua file di folder src!
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL,
   },
-  verbose: true,
-  strict: true,
 });
