c4a9ba18f2b280810627d8a1a9fe53140aa011ef
Ghiyas Dzul hilman <ghiyasdzulhilmansh@gmail.com>
2026-07-02 04:51:31 +0700

    Delete operasionalFeed route index.ts

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -9,7 +9,6 @@ import operasionalRoutes from "./operasional";
 import mappingsRouter from "./mappings";
 import dashboardRouter from "./dashboard";
 import stagingRouter from "./staging";
-import operasionalFeedRouter from "./operasionalFeed";
 import produkRoutes from "./produk";
 
 const router: IRouter = Router();
@@ -24,7 +23,6 @@ router.use(operasionalRoutes);
 router.use(mappingsRouter);
 router.use(dashboardRouter);
 router.use(stagingRouter);
-router.use(operasionalFeedRouter);
 router.use(produkRoutes);
 
 export default router;
